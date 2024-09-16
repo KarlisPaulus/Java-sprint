8acf72c4f0e20444e943c451002787ee7e7a5e45
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-16 17:34:52 +0300

    commit

---
+++ AsciiAdder.java
@@ -0,0 +1,9 @@
+package sprint;
+
+public class AsciiAdder {
+
+    public char addAscii(char input, int intValue) {
+        char result = (char) (input + intValue); // result outputs inputed char moved by the int value.
+        return result;
+    }
+}
