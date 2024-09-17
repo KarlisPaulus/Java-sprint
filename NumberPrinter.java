0b79a0849324462642864930302178ebc8905e64
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-17 22:22:32 +0300

    add negative case

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -13,6 +13,9 @@ package sprint;
 
 public class NumberPrinter {
     public void printNums(int n) {
+        if (n < 0) {
+            System.out.println("Negative numbers are not allowed");
+        }
         StringBuilder numbers = new StringBuilder();
             for (int i = 0; i <= n; i++) {
                 numbers.append(i).append("\n");
