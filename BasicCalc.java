c13a068f536e1c0ce6c01eb68da6ddc71c38601e
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-17 12:36:03 +0300

    commit

---
+++ BasicCalc.java
@@ -0,0 +1,27 @@
+package sprint;
+
+public class BasicCalc {
+    public int doOperation(int a, char op, int b) {
+        if (op == '+') {
+            return a + b;
+        } else if (op == '-') {
+            return a - b;
+        } else if (op == '/') {
+            if (b != 0) {
+                return a / b;
+            } else {
+                return 0;
+            }
+        } else if (op == '*') {
+            return a * b;
+        } else if (op == '%') {
+            if (b != 0) {
+                return a % b;
+            } else {
+            return a % b;
+            }
+        } else {
+            return 0;
+        }
+    }
+}
