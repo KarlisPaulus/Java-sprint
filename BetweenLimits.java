5af5e4071e8cee245ae05519a3e64e95e3e397bc
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-17 16:53:32 +0300

    add comments

--- BetweenLimits.java
+++ BetweenLimits.java
@@ -11,12 +11,12 @@ public class BetweenLimits {
             from = to;
             to = flip;
         }
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder(); // stringbuilder is used to add chars to string. in golang we used string+=i but in java it doesn't work.
 
         for (char i = (char) (from + 1); i < to; i++) {
-            result.append(i);
+            result.append(i);   // lisama
         }
         
-        return result.toString();
+        return result.toString();   // convert StringBuilder to String.
     }
 }
