d7d97ca6b697c8104d0850738d81d1a3d05a9651
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-18 20:27:54 +0300

    initial commit. no negative case fix

---
+++ DigitSum.java
@@ -0,0 +1,18 @@
+package sprint;
+
+public class DigitSum {
+    public static int sumOfDigits(int number) {
+        String strNumbers = String.valueOf(number); // "1234"
+
+        int result = 0;
+        for (int i = 0; i < strNumbers.length(); i++) {     // loops until length of input. "<"" need to be because we start cunting from 0.
+            char currentChar = strNumbers.charAt(i); // gets current character in ascii value of string
+            int numericValue = Character.getNumericValue(currentChar);  // need to convert the ascii value to numeric
+            result += numericValue; // add to the result. in my case 1+2+3+4=10
+
+        }
+        return result;
+    }
+    
+}
+    
