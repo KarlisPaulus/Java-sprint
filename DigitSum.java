2b38a0ac5c73042d511d723cbdf9d887d25326c5
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-18 21:16:14 +0300

    negative case fix

--- DigitSum.java
+++ DigitSum.java
@@ -2,9 +2,16 @@ package sprint;
 
 public class DigitSum {
     public static int sumOfDigits(int number) {
-        String strNumbers = String.valueOf(number); // "1234"
-
         int result = 0;
+
+        String strNumbers = String.valueOf(number); // "1234"
+        char charNumber = strNumbers.charAt(0);
+        if (charNumber == '-') {
+            result = 1;
+        } else {
+            result = 0;
+        }
+
         for (int i = 0; i < strNumbers.length(); i++) {     // loops until length of input. "<"" need to be because we start cunting from 0.
             char currentChar = strNumbers.charAt(i); // gets current character in ascii value of string
             int numericValue = Character.getNumericValue(currentChar);  // need to convert the ascii value to numeric
