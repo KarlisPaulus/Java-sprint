1f35b5fc09a37255a164097b3a7bad2e529e6ff7
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-18 12:42:39 +0300

    initial commit

---
+++ Accumulator.java
@@ -0,0 +1,15 @@
+package sprint;
+
+public class Accumulator {
+    public int accumulate(int n) {
+
+    if (n < 0) {
+        return 0;
+    }
+    int result = 0; // need to give some value to it "result = 0"
+    for (int i = 0; i <= n; i++) {
+        result += i;
+    }
+    return result;
+}
+}
