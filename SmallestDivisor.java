ea0a23cbc48a8e99dc066941ceb1d5bed522c3ca
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-19 14:32:43 +0300

    fix

--- SmallestDivisor.java
+++ SmallestDivisor.java
@@ -3,7 +3,7 @@ package sprint;
 public class SmallestDivisor {
     public int smallestDivisor(int number) {
         int divisor = 0;
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= number; i++) {
             if (number%i == 0) {
                 divisor = i;
                 break;
