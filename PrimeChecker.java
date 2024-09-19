a3ee4f4ce74782fb9decac4fb3a1b0469c9f64a9
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-19 11:27:47 +0300

    fix edge cases and add comments

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -4,7 +4,7 @@ public class PrimeChecker {
     public static boolean isPrime(int number) {
         boolean result = true;
         
-        for (int i = 2; i < (int)Math.sqrt(number); i++) {
+        for (int i = 0; i < (int)Math.sqrt(number); i++) {
 
             if (number%i == 0) {
                 result = false;
