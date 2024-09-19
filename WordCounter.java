688d6aa58b7c0e7e9a7bdd9dfc60e9367a96714a
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-19 21:07:52 +0300

    comments

--- WordCounter.java
+++ WordCounter.java
@@ -6,16 +6,16 @@ public class WordCounter {
             int wordCount = 0;
             boolean isWord = false;
         for (int i = 0; i < sentence.length(); i++) {
-            //char currentchar = sentence.charAt(i)
-            if (Character.isLetter(sentence.charAt(i))) {
-                isWord = true;
+            //char currentchar = sentence.charAt(i) would be better
+            if (Character.isLetter(sentence.charAt(i))) {   // if current char is letter
+                isWord = true;  // it will be word
             }
-            if (Character.isWhitespace(sentence.charAt(i)) && isWord) {
-                    wordCount ++;
-                    isWord = false;
+            if (Character.isWhitespace(sentence.charAt(i)) && isWord) { // if currentchar is space and isWord is true from previous control
+                    wordCount ++;   //then the word is ended
+                    isWord = false; // make the isWord to false for finding the next word
                  }
             } 
-            if (isWord) {
+            if (isWord) {   // this is for word when it will be last(without space)
                 wordCount++;
             }
             return wordCount;
