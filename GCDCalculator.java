6ef19a36f8ed79779e3358ea218cfbf6cc713bff
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-20 18:32:20 +0300

    initial commit

---
+++ GCDCalculator.java
@@ -0,0 +1,14 @@
+package sprint;
+
+public class GCDCalculator {
+    
+    public int gcd(int a, int b) {
+
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+}
