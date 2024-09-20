93c543da0fbb9979167ab272651f0a3d8b172fac
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-20 21:29:33 +0300

    initial commit

---
+++ ArrayInitializer.java
@@ -0,0 +1,16 @@
+package sprint;
+
+public class ArrayInitializer {
+    public int[] fillArray(int max) {
+        int[] emptyArray = new int [0];
+        if (max < 1) {
+            return emptyArray;
+        }
+        
+        int[] array = new int[max];
+        for (int i = 0; i < array.length; i++) {
+            array[i] += i;
+        }
+        return array;
+    }
+}
