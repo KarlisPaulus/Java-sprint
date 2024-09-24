0e9580ca750754096b5f02b88ce22da037cf86f2
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-24 20:26:39 +0300

    Initial commit

--- ArrayAdder.java
+++ ArrayAdder.java
@@ -4,6 +4,7 @@ public class ArrayAdder {
         int[] result = new int[arr1.length + arr2.length];
         System.arraycopy(arr1, 0, result, 0, arr1.length);
         System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+
         return result;
     }
 }
