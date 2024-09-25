50ce639430cb723c11f8fe10184f66a90c5951d4
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-25 17:59:45 +0300

    empty list fix

--- ListManipulator.java
+++ ListManipulator.java
@@ -1,20 +1,22 @@
 package sprint;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListManipulator {
     public List<String> manipulateList(List<String> list) {
         if (list.isEmpty()) {
-            list = new ArrayList<String>();
+            list.set(0, "1");
             return list;
-        }
-        list.removeLast(); // Arraylist size automatically extends. Array doesn't
-        int last = list.size()-1;
-        list.set(last,"The size of the list is " + list.size());
-        list.addLast("last");
-        list.set(0,"first");
+        } else {
+            list.removeLast(); // Arraylist size automatically extends. Array doesn't
+            int last = list.size() - 1;
+            list.set(last, "The size of the list is " + list.size());
+            list.addLast("last");
+            list.set(0, "first");
 
+        }
         return list;
     }
 }
