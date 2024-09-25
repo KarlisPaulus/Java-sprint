142c2b3bb30f820fc029eb668dc15984606a8e9f
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-25 22:11:40 +0300

    single element fix

--- ListManipulator.java
+++ ListManipulator.java
@@ -8,15 +8,15 @@ public class ListManipulator {
     public List<String> manipulateList(List<String> list) {
         if (list.isEmpty()) {
             list.add("1");
-            return list;
+        } else if (list.size() == 1) {
+            list.set(0, "first");
         } else {
             list.removeLast(); // Arraylist size automatically extends. Array doesn't
             int last = list.size() - 1;
             list.set(last, "The size of the list is " + list.size());
             list.addLast("last");
             list.set(0, "first");
-
         }
-        return list;
+            return list;
     }
 }
