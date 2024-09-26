042b3f48ca8af909ab416e7a249f360fca596f7e
Karlis Paulus <pauluskarlis@gmail.com>
2024-09-26 12:06:19 +0300

    initial commit

---
+++ ArrayModifier.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+public class ArrayModifier {
+    public static ArrayList<Double> removeElementsBetween(ArrayList<Double> list, int index1, int index2) {
+        
+        if (index1 > index2) {
+            int swap = index1;
+            index1 = index2;
+            index2 = swap;
+        }
+
+        index1 = Math.max(0, index1);
+        index2 = Math.min(list.size(), index2);
+
+        for (int i = index1; i < index2; i++) {
+            list.remove(index1);
+        }
+
+        return list;
+    }
+}
+
