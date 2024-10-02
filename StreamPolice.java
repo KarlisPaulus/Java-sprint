f99739392cd46ad2365a75f75d816be8e8c1ec84
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-02 21:28:50 +0300

    fix

--- StreamPolice.java
+++ StreamPolice.java
@@ -5,11 +5,9 @@ import java.util.stream.Collectors;
 
 public class StreamPolice {
     public List<Integer> processNumbers (List<Integer> numbers) {
-        List<Integer> result = numbers.parallelStream()
+        return numbers.stream()
         .filter(n -> n >= 0)
         .filter(n -> !(n % 5 == 0 && n % 10 != 0))
         .collect(Collectors.toList());
-
-        return result;
     }
 }
