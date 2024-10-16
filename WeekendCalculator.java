cd69dc175e9e979abb1dc0db89a13e5c58b0392f
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-16 14:20:55 +0300

    logic fix

--- WeekendCalculator.java
+++ WeekendCalculator.java
@@ -6,22 +6,21 @@ import java.time.DayOfWeek;
 public class WeekendCalculator {
     public static long countWeekendDays (LocalDate startDate, LocalDate endDate) {
         long result = 0;
+
         DayOfWeek days = startDate.getDayOfWeek();
-        int dayNum = days.getValue();
+        int currentDay = days.getValue();
         int startDay = startDate.getDayOfYear();
         int endDay = endDate.getDayOfYear();
-        int weeks = (endDay - startDay) / 7;    // (278-248)/7 to count weeks
-        int times = 0;
-        for (int day = dayNum; times <= weeks; day++) {
-
-            if (day == 6) {
+        int totalDays = endDay - startDay;
+        for (int day = 0; day <= totalDays; day++) {
+            if (currentDay == 6 || currentDay == 7) {
                 result += 1;
-            } else if (day == 7) {
-                result += 1;
-                day = 1;
-                times += 1;
             }
+            currentDay += 1;
+            if (currentDay > 7) {
+                currentDay = 1;
+            } 
         }
-        return result-1;
+        return result;
     }
 }
