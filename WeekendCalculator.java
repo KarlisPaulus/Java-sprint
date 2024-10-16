61d6ca291d634016d268c3bf9d2d54ffc5539fbf
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-16 13:22:19 +0300

    initial commit

---
+++ WeekendCalculator.java
@@ -0,0 +1,27 @@
+package sprint;
+
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+
+public class WeekendCalculator {
+    public static long countWeekendDays (LocalDate startDate, LocalDate endDate) {
+        long result = 0;
+        DayOfWeek days = startDate.getDayOfWeek();
+        int dayNum = days.getValue();
+        int startDay = startDate.getDayOfYear();
+        int endDay = endDate.getDayOfYear();
+        int weeks = (endDay - startDay) / 7;    // (278-248)/7 to count weeks
+        int times = 0;
+        for (int day = dayNum; times <= weeks; day++) {
+
+            if (day == 6) {
+                result += 1;
+            } else if (day == 7) {
+                result += 1;
+                day = 0;
+                times += 1;
+            }
+        }
+        return result-1;
+    }
+}
