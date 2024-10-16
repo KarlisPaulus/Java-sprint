c684ed82c7231dabe9a5da84c573de5c36ca32c6
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-16 21:11:32 +0300

    bug fixes

--- Factorial.java
+++ Factorial.java
@@ -2,10 +2,13 @@ package sprint;
 
 public class Factorial {
     public int calculateFactorial(int n) {
-       if (n <= 0) {
+        if (n < 0) {    // when input is negative
+            return 0;
+        }
+       if (n <= 1) {
         return 1;
        }
        // recursive call
-       return n * calculateFactorial(n-1);  // this keeps calling itself until n <= 0
+       return n * calculateFactorial(n-1);  // this keeps calling itself until n <= 1
     }
 }
