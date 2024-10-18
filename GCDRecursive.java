1579d885083c993c778afa5909b64c73ec673912
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-18 13:46:44 +0300

    negative case fix

--- GCDRecursive.java
+++ GCDRecursive.java
@@ -2,9 +2,17 @@ package sprint;
 
 public class GCDRecursive {
     public static int gcd (int num1, int num2) {
+        if (num2 < 0) {
+        num2 = num2 * -1;
+        }
+        if (num1 < 0) {
+            num1 = num1* -1;
+        }
+
         if (num2 <= 1) {
             return num1;
-        }
+        } 
+       
         return gcd(num2, num1 % num2);
     }
 }
