ab53bc1e14b14213a2d504f21a22ac9f851b8f7a
Karlis Paulus <pauluskarlis@gmail.com>
2024-10-21 17:39:52 +0300

    initial commit

---
+++ OccurrenceCounter.java
@@ -0,0 +1,23 @@
+package sprint;
+
+public class OccurrenceCounter {
+    public int countOccurrences (int[] arr, int element, int index) {
+        
+        if (arr == null) {
+            return 0;
+        }
+        return countOccurrencesHelper(arr, element, index);
+    }
+    private int countOccurrencesHelper (int[] arr, int element, int index) {
+        if (index >= arr.length) {
+            return 0;
+        }
+        int result;
+        if (arr[index] == element) {    // checking match
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result + countOccurrencesHelper(arr, element, index + 1);    // adds to result
+    }
+}
